3246c9464250e672e95b7b809b271466c7ba28c8
Vishruth <notvishruth@gmail.com>
2026-06-26 14:35:58 +0530

    Added documentation to ClothingItem

--- app/src/main/java/com/example/fitfusion/data/entity/ClothingItem.kt
+++ app/src/main/java/com/example/fitfusion/data/entity/ClothingItem.kt
@@ -12,3 +12,13 @@ data class ClothingItem(
     val color: String,
     val material: String
 )
+/*
+    uri in imageUri stands for Uniform Resource Identifier
+    It is a pointer to a location in memory where the image is actually stored
+    This URI object is handed by the Android OS when the user selects an image from the gallery
+
+    Now the image is stored as a string because we use room which is a wrapper over SQLite. SQLite
+    is lightweight, relational database and it only understands primitive data types
+    It does not know what a URI object is, while it could be stored as raw binary data,
+    doing so would drastically bloat our db and therefore we store the address as a text
+ */
