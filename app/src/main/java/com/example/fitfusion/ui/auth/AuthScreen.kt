e671b21c6d8d3e3fb79fa2e1260de60a6f78ed86
Vishruth <notvishruth@gmail.com>
2026-09-09 23:37:08 +0530

    Building Login Screen

--- app/src/main/java/com/example/fitfusion/ui/auth/AuthScreen.kt
+++ app/src/main/java/com/example/fitfusion/ui/auth/AuthScreen.kt
@@ -7,11 +7,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,7 +44,23 @@ fun AuthScreen(
                 .padding(32.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            // 1. Declare the hoisted state variables at the top of AuthScreen
+            var username by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
 
+            OutlinedTextField(
+                value = username,
+                onValueChange = { newText -> username = newText },
+                label = { Text("Username") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
